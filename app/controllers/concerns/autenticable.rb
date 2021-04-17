84e630b72c5d9925effca6ff74c73d7e84454c60
betopinheiro1005 <betopinheiro1005@yahoo.com.br>
2021-04-17 20:46:22 -0300

    Alterações no arquivo autenticable.rb

--- app/controllers/concerns/autenticable.rb
+++ app/controllers/concerns/autenticable.rb
@@ -5,11 +5,8 @@ module Autenticable
     @token ||= request.headers['Authorization']
 
     unless valid_token?
-
       render json: { errors: 'Forneça um header Authorization para se identificar (qualquer um com pelo menos 10 caracteres).' },
-
              status: :unauthorized
-
     end
   end
 
